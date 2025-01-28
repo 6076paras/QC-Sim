25d42a4d32f37585adf3012f5364d98b7121f74b
6076paras <47757847+6076paras@users.noreply.github.com>
2025-01-27 23:19:25 -0800

    container elemts

--- include/QuantumCircuits.hpp
+++ include/QuantumCircuits.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+#include <vector>   
 /**
  * @brief Quantum Circuit class that acts as container 
  * Provides methods to:
@@ -7,14 +9,21 @@
  * - Display circuit diagram
  * - Execute quantum operations
  */
-class QuantumCircuit{
+class QuantumCircuit {
     
     public:
+        explicit QuantumCircuit(int nQubits); 
         int nQubit;
         void H(int qubit);
         void Z(int qubit);
         void X(int qubit);
         void CNOT(int control, int target);
         void display();
+        struct unitOperator {
+            std::string gateName;
+            int target;
+            int control = -1;
+        };
+        std::vector<unitOperator> container;
     private:
 };
--- src/QuantumCircuit.cpp
+++ src/QuantumCircuit.cpp
@@ -1,5 +1,28 @@
 #include "QuantumCircuits.hpp"
+#include <iostream>
+#include <vector>
 
-#pragma once
+// initialize a circuit with n qubits
+QuantumCircuit::QuantumCircuit(int nQubits) {
+    this->nQubit = nQubits;
+} 
 
-QuatumCircuits::QuantumCircuits
+// add H gate to the container
+void QuantumCircuit::H(int qubit){
+    this->container.push_back({"H",qubit}); 
+}
+
+// add X gate to the container
+void QuantumCircuit::X(int qubit){
+    this->container.push_back({"X",qubit}); 
+}
+
+// add Z gate to the container
+void QuantumCircuit::Z(int qubit){
+    this->container.push_back({"Z",qubit}); 
+}
+
+// add Z gate to the container
+void QuantumCircuit::CNOT(int target, int control){
+    this->container.push_back({"Z", target, control}); 
+}
