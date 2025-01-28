e84e073535d2cc98f6d9c3dd19ed2f2f4a8ffa6e
6076paras <47757847+6076paras@users.noreply.github.com>
2025-01-27 23:56:32 -0800

    circuit drawing implemntation

--- src/QuantumCircuit.cpp
+++ src/QuantumCircuit.cpp
@@ -26,3 +26,40 @@ void QuantumCircuit::Z(int qubit){
 void QuantumCircuit::CNOT(int target, int control){
     this->container.push_back({"Z", target, control}); 
 }
+
+// display the container visually
+void QuantumCircuit::display(){
+
+    // loop throguh "a" qubit, and add operators belonging to it
+    for (int q = 1; q == this->nQubit + 1 ; q++){
+            
+            // lopp through gates and Print gates beloning to a qubit
+            for (auto& op : container){
+                if (op.target == q || op.control == q) {
+                    if (op.gateName == "H") {
+                        std::cout << "─[H]─";
+                    }
+                    else if (op.gateName == "X") {
+                        std::cout << "─[X]─";
+                    }
+                    else if (op.gateName == "Z") {
+                        std::cout << "─[Z]─";
+                    }
+                    else if (op.gateName == "CNOT") {
+                        if (q == op.control) {
+                            std::cout << "──●──";  
+                        }
+                        else if (q == op.target) {
+                             std::cout << "──⊕──";  
+                        }
+                    }
+                }
+                // print empty line  
+                else {
+                std::cout << "─────";  
+                }
+
+            };
+    };
+
+}
